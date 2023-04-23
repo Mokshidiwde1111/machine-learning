30662aaa3e6dc7289919d907f907226ed17dd9b0
Mokshi Diwde <119008927+Mokshidiwde1111@users.noreply.github.com>
2023-04-23 17:18:35 +0530

    Create Experiment_2.py

---
+++ Experiment_2.py
@@ -0,0 +1,14 @@
+# Mokshi Diwde 0827CI201111
+#List Comprehension 
+ number_list = [ x for x in range(20) if x % 2 == 0] print(number_list) 
+# Mokshi Diwde 0827CI201111
+#List Comprehension 
+ 
+lettersList = list(map(lambda x: x, 'human')) print(lettersList) 
+# Mokshi Diwde 0827CI201111  
+#List Comprehension 
+ 
+num_list = [y for y in range(100) if y % 2 == 0 if y % 5 == 0] print(num_list) 
+# Mokshi Diwde 0827CI201111
+#List Comprehension 
+ obj = ["Even" if i%2==0 else "Odd" for i in range(10)] print(obj)  
